592d15c05f101ca5b032ecc5da15afc9f3b88052
PavelPustovoit <90083675+PavelPustovoit@users.noreply.github.com>
2021-12-11 21:13:07 +0200

    Add files via upload

---
+++ SelenTask/LoginAuto.java
@@ -0,0 +1,33 @@
+package SelenTask;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class LoginAuto {
+
+
+    public static WebDriver login(WebDriver driver, List<String> args) {
+
+        driver.get(args.get(0));
+        WebElement username=driver.findElement(By.id("txtUsername"));
+        WebElement password=driver.findElement(By.id("txtPassword"));
+        WebElement login=driver.findElement(By.id("btnLogin"));
+        username.sendKeys(args.get(1));
+        password.sendKeys(args.get(2));
+        login.click();
+        String expectedUrl = args.get(3);
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl,actualUrl);
+        return driver;
+    }
+
+    public static void close(WebDriver driver){
+        driver.close();
+    }
+}
---
+++ SelenTask/OpenDriver.java
@@ -0,0 +1,16 @@
+package SelenTask;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenDriver {
+
+    public static WebDriver open(){
+
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
